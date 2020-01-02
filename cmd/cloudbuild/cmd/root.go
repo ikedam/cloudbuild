fdeb2a20657c8161d1579333fbfaef1be2bc53dd
ikedam <devld@ikedam.jp>
2020-01-02 17:18:39 +0900

    Improve copyrights

--- cmd/cloudbuild/cmd/root.go
+++ cmd/cloudbuild/cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 IKEDA Yasuyuki
+Copyright 2020 IKEDA Yasuyuki
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
